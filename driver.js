2bc4862f10923a431dbbecc25fa100b9d088b0b9
Yoan Ante <yoanante@gmail.com>
2015-09-25 11:48:49 -0400

    added blackjack function to dealer.js test to see if push or winner in driver.js with blackjack.

--- driver.js
+++ driver.js
@@ -8,6 +8,16 @@ var blackjack = function () {
   dealer.hit(deck.dealCard())
   player.hit(deck.dealCard())
   dealer.hit(deck.dealCard())
+
+  if (player.blackjack() && dealer.blackjack() === false){
+    console.log("player wins BLACKJACK")
+    //add money to player purse
+  }
+    else if(player.blackjack() && dealer.blackjack()){
+      console.log("PUSH BOTH HAVE BLACK JACK")
+    //player purse stays the same
+    }
+
   player.calculate()
 
   console.log('player is holding ' + player.hand[0].name + ' and ' + player.hand[1].name)
@@ -15,8 +25,5 @@ var blackjack = function () {
   console.log('player is betting $' + player.bet)
   console.log('player has $' + player.purse)
   console.log("value of player's hand: " + player.calculate())
-  //  player hand value compare to 21 if == 21 check dealer if dealer not 21 win 1.5
-  //  else if > 21 bust  else ask hit or stand
-  //  dealer if value under 17 hit until 17 or bust. Figure ace 1 or 11 for both somehow
 }
 blackjack()
